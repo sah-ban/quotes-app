1e38031e2bbd25bcf8fb036c043f61c4937b0bd0
sahban <sahbaan@proton.me>
2025-10-05 20:46:59 +0530

    update connector

--- src/components/providers/WagmiProvider.tsx
+++ src/components/providers/WagmiProvider.tsx
@@ -1,14 +1,14 @@
 import { createConfig, http, WagmiProvider } from "wagmi";
 import { arbitrum } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
+import { farcasterMiniApp } from "@farcaster/miniapp-wagmi-connector";
 
 export const config = createConfig({
   chains: [arbitrum],
   transports: {
     [arbitrum.id]: http(),
   },
-  connectors: [farcasterFrame()],
+  connectors: [farcasterMiniApp()],
 });
 
 const queryClient = new QueryClient();
